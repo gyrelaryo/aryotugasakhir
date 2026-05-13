d6b532c2081df48e5d2163ce4c44f30b2761d41b
gyrelaryo <gyrelaryo.infor@gmail.com>
2026-05-13 09:22:33 +0700

    Update welcome.js

--- login/welcome.js
+++ login/welcome.js
@@ -1,1 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
 
+    const isLogin = localStorage.getItem('isLogin');
+    const username = localStorage.getItem('username');
+
+    const loginMenu = document.querySelector(
+        'a[href="login/index.html"]'
+    );
+
+    if (isLogin === 'true' && loginMenu) {
+
+        loginMenu.innerHTML = `Halo, ${username}`;
+
+        loginMenu.href = "#";
+
+        // tombol logout
+        loginMenu.addEventListener('click', () => {
+
+            const confirmLogout =
+                confirm('Yakin ingin logout?');
+
+            if (confirmLogout) {
+
+                localStorage.removeItem('isLogin');
+                localStorage.removeItem('username');
+
+                window.location.reload();
+            }
+        });
+    }
+});
